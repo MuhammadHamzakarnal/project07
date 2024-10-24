1b3fce2bc22ba4a282b36813c2cbec2a2af5676c
MuhammadHamzakarnal <0310muhammadhamza356@gmail.com>
2024-10-23 23:31:30 -0700

    Assignment done

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    //  image code
+    <div className="bg-[url('/image/next.png')] bg-no-repeat  h-screen bg-cover ">
+      <h2 className='p-[20px]  mx-[500px] text-[36px] text-white text-center bg-primary'>hello World</h2><br />
+      <h3 className='bg-secendory p-5'>Hello pakistan</h3><br />
+      <h4 className='bg-hamza p-5'>hello hamza</h4>
+      </div>
+  )
+}
+
+export default Homepage
---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: "#f6bf38",
+        secendory:"#ff6600",
+        hamza:"#ff6632"
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
